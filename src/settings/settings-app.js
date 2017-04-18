8f4c72c67a798d7f8b67557c0ceaae23fb3aa3b0
AnAbstractThought <joseph_mayberry@compaid.com>
2017-04-18 09:37:26 -0400

    Revert "update to Rise Vision time-date widget"
    This reverts commit eaebcb1efad79a0d2b59851c1948fe58acd19859.

--- src/settings/settings-app.js
+++
@@ -1,8 +0,0 @@
-angular.module("risevision.widget.timedate.settings", [
-  "risevision.common.i18n",
-  "risevision.widget.common",
-  "risevision.widget.common.widget-button-toolbar",
-  "risevision.widget.common.tooltip",
-  "risevision.widget.common.font-setting"
-]);
-
